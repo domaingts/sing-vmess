5081a108e27258b0e8e3665adb487131e80a08fd
domaingts <139693268+domaingts@users.noreply.github.com>
2025-08-21 20:46:39 +0800

    Update vision_utls.go

--- vless/vision_utls.go
+++ vless/vision_utls.go
@@ -16,13 +16,13 @@ import (
 
 func init() {
 	tlsRegistry = append(tlsRegistry, func(conn net.Conn) (loaded bool, netConn net.Conn, reflectType reflect.Type, reflectPointer uintptr) {
-		uConn, loaded := common.Cast[*utls.UConn](conn)
-		if loaded {
-			return true, uConn.NetConn(), reflect.TypeOf(uConn.Conn).Elem(), uintptr(unsafe.Pointer(uConn.Conn))
-		}
 		tlsConn, loaded := common.Cast[*ec.Conn](conn)
 		if loaded {
 			return true, tlsConn.NetConn(), reflect.TypeOf(tlsConn).Elem(), uintptr(unsafe.Pointer(tlsConn))
+		}
+		uConn, loaded := common.Cast[*utls.UConn](conn)
+		if loaded {
+			return true, uConn.NetConn(), reflect.TypeOf(uConn.Conn).Elem(), uintptr(unsafe.Pointer(uConn.Conn))
 		}
 		return
 	})
